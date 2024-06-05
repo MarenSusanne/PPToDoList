bf6f6c4576e74e4d69f9710c4809167cf1553221
MarenSusanne <Marensut@getacademy.no>
2024-06-05 11:23:55 +0200

    Add project files.

---
+++ PPToDoList/Program.cs
@@ -0,0 +1,38 @@
+﻿// Lag en to do app! Du skal kunne legge til oppgaver, slette dem og gå inn på en task for å se beskrivelse eller flere detaljer
+// List m/oppgaver
+using PPToDoList;
+ThingsToDo newTask = new ThingsToDo();
+Main();
+
+void Main()
+{
+    while (true) {
+    Console.Clear();
+    newTask.ShowTasks();
+    Console.WriteLine("\nOptions:");
+    Console.WriteLine("1. Add a new Task to your list!");
+    Console.WriteLine("2. Remove a task from your list!");
+    Console.WriteLine("3. Read more info on the task");
+
+    string Response = Console.ReadLine();
+    int Check = Convert.ToInt32(Response);
+
+        switch (Check)
+        {
+            case 1:
+                newTask.AddNewTasks();
+                break;
+            case 2:
+                newTask.DeleteTasks();
+                break;
+            case 3:
+                newTask.InfoOnTask();
+                break;
+            default:
+                Console.WriteLine("Please input a valid option!");
+                break;
+        }
+
+    }
+
+}
---
+++ PPToDoList/Tasks.cs
@@ -0,0 +1,89 @@
+﻿using System.Threading.Tasks;
+
+namespace PPToDoList;
+
+public class ThingsToDo
+{
+    List<ThingsToDo> tasks = new List<ThingsToDo>();
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public ThingsToDo()
+    {
+
+    }
+
+    public ThingsToDo(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public void addTask()
+    {
+
+        tasks.Add(new ThingsToDo("Laundry", "do laundry"));
+        tasks.Add(new ThingsToDo("Clean", "sort living room"));
+
+        foreach (ThingsToDo thingsToDo in tasks)
+        {
+            Console.WriteLine("This is your tasks:\n" + thingsToDo.Name);
+        }
+    }
+
+    public void ShowTasks()
+    {
+        Console.WriteLine("Theese are your tasks that need doing!");
+        foreach (ThingsToDo thingsToDo in tasks)
+        {
+            Console.WriteLine($"\t {thingsToDo.Name}");
+        }
+
+    }
+
+    public void AddNewTasks()
+    {
+        Console.WriteLine("Write the name of your task:");
+        var nameResponse = Console.ReadLine();
+        Console.WriteLine("Write a description:");
+        var descriptionResponse = Console.ReadLine();
+        tasks.Add(new ThingsToDo(nameResponse, descriptionResponse));
+    }
+
+    public void DeleteTasks()
+    {
+        Console.WriteLine("Please write the name of the task to remove.");
+        var TaskToRemove = Console.ReadLine();
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            if (tasks[i].Name == TaskToRemove)
+            {
+                tasks.RemoveAt(i);
+            }
+        }
+    }
+
+    public void InfoOnTask()
+    {
+        bool Exit = true;
+        while (Exit)
+        {
+            Console.WriteLine("write the name of a task to see the description or exit to return to menu.");
+            var TaskInfo = Console.ReadLine();
+            for (int i = 0; i < tasks.Count; i++)
+            {
+                if (tasks[i].Name == TaskInfo)
+                {
+                    Console.WriteLine($"Name of task: {tasks[i].Name}");
+                    Console.WriteLine($"Description: {tasks[i].Description}");
+
+                }
+                else if (TaskInfo == "exit") { Exit = false; }
+            }
+        }
+    }
+}
+
+
+
+
